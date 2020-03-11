0de179022897a560e931fc5194d11f1d46b639e4
Valdemar Ceccon <valdemar.ceccon@objective.com.br>
2020-03-11 15:02:16 -0300

    started to work with the upload

--- cmd/gedserver/main.go
+++ cmd/gedserver/main.go
@@ -1,14 +1,64 @@
 package main
 
 import (
+	"github.com/gorilla/mux"
+	"io"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
-	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
-		writer.Write([]byte("<h1>Ola</h1>"))
-	})
+	r := mux.NewRouter()
 
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	r.HandleFunc("/", test)
+	r.HandleFunc("/upload", upload)
+
+	log.Fatal(http.ListenAndServe(":8080", r))
+}
+
+func test(w http.ResponseWriter, r *http.Request) {
+	_, err := w.Write([]byte("hello world"))
+
+	if err != nil {
+		log.Println(err.Error())
+	}
+}
+
+func upload(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		_, err := w.Write([]byte("only post allowed here"))
+
+		if err != nil {
+			log.Println(err.Error())
+		}
+		return
+	}
+
+	defer r.Body.Close()
+	f, err := os.OpenFile("a.png", os.O_CREATE | os.O_RDWR, 0755)
+
+	if err != nil {
+		log.Println(err.Error())
+	}
+
+	r.Body = http.MaxBytesReader(w, r.Body, 2000000000000)
+
+	if err = r.ParseMultipartForm(2000000000000); err != nil {
+		log.Println(err)
+		return
+	}
+
+	file, _, err := r.FormFile("file")
+	if  err != nil {
+		log.Println(err)
+		return
+	}
+	io.Copy(f, file)
+
+	if err != nil {
+		log.Println(err.Error())
+	}
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("upload ok"))
 }
